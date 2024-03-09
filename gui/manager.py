6394e047a3a2703a8bd49ddab9e2d03fb1160ab3
Joe Williams <joe.williams@oit.edu>
2024-03-08 20:36:39 -0800

    who needs guis anyway lol

--- gui/manager.py
+++
@@ -1,8 +0,0 @@
-''' --- manages an entire gui --- '''
-import pygame
-
-class GUI_Manager:
-  elements: list=[]
-
-  def update_text(self, text: str):
-    pass
--- modules/inventory.py
+++
@@ -1,5 +0,0 @@
-''' --- a module that tracks a player's inventory --- '''
-
-class Item:
-  name: str
-  count: int
